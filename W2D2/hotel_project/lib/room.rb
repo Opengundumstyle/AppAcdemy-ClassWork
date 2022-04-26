6e87048140f5e9899a416e43a97fd6f6d5a2457c
Zhile Lin <zhlin04@gmail.com>
2022-04-26 14:21:28 -0700

    finished first part

--- W2D2/hotel_project/lib/room.rb
+++ W2D2/hotel_project/lib/room.rb
@@ -1,3 +1,57 @@
 class Room
-  
+#PART 1
+ #initialize
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+#capacity
+ def capacity
+    @capacity
+ end
+
+ #occupants
+  def occupants
+    @occupants
+  end
+
+  def full?
+     if @occupants.length < @capacity
+
+        return false
+     else
+        return true
+     end
+  end
+
+def available_space
+
+   return @capacity - @occupants.length
+
+end
+
+def add_occupant(name)
+
+   if full?
+
+      return false
+
+   else
+      
+      @occupants << name
+
+      return true
+
+   end
+
+end
+
+
+#PART 2
+
+
+
+
+
 end
