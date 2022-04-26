2cdd4a9f4af2e3a4fd047147e0eef30c30a6892e
Zhile Lin <zhlin04@gmail.com>
2022-04-26 14:03:49 -0700

    finished with hangman

--- W2D2/hangman_project 2/lib/hangman.rb
+++ W2D2/hangman_project 2/lib/hangman.rb
@@ -48,6 +48,76 @@ class Hangman
       end
 
 
+    #Part 2
+    # try_guess 
+     def try_guess(char)
+      if already_attempted?(char)
+         p "that has already been attempted"
+         return false
+
+      else
+
+         @attempted_chars << char
+         
+
+            if @secret_word.include?(char) 
+
+                idexArr = get_matching_indices(char)
+                  
+                fill_indices(char,idexArr)
+            else
+
+              @remaining_incorrect_guesses -= 1
+              
+            end
+
+        return true
+
+      end
+      
+     end
+
+
+ #ask_user_for_guess
+    def ask_user_for_guess
+        p 'Enter a char:'
+        guess = gets.chomp
+        try_guess(guess)
+    end
+  
+# win ?
+   def win?
+    if @guess_word.join("") == @secret_word
+        p 'WIN'
+        return true
+    else
+       return false
+    end
+   end
+
+# lose?
+   def lose?
+      if @remaining_incorrect_guesses == 0 
+         p "LOSE"
+         return true
+      else
+        return false
+      end
+   end
+
+# game over?
+
+def game_over?
+   if win? || lose?
+       p @secret_word
+       return true
+   else
+      return false
+   end
+end
+
+
+
 end
 
 
