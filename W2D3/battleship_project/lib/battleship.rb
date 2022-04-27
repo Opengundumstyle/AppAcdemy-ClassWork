d81b734f8d86245892daad742acade95124d4046
Zhile Lin <zhlin04@gmail.com>
2022-04-27 13:40:44 -0700

    finish

--- W2D3/battleship_project/lib/battleship.rb
+++ W2D3/battleship_project/lib/battleship.rb
@@ -1,7 +1,14 @@
 require_relative "board"
 require_relative "player"
 
+require "byebug"
+
+
 class Battleship
+
+
+
+    #initialize
    def initialize(n)
        
        @player = Player.new
@@ -11,9 +18,62 @@ class Battleship
        
    end
     
+   def board
+       @board
+   end
     
+   def player
+      @player
+   end
+  
+   def start_game
+        @board.place_random_ships
+        puts @board.num_ships
+        puts @board.print
+        
+   end
 
+   def lose?
+       if @remaining_misses <= 0
+            puts "you lose"
+            return true
+       else
+           return false
+       end
+   end
+   
+def win?
+     if @board.num_ships == 0
+        puts "you win"
+        return true
+    else
+        return false
+    end
+end
 
+def game_over?
+
+    return true if win? || lose?
+    false
+    
+end
+
+def turn 
+
+   playerInput = @player.get_move
+#    debugger
+   
+     
+    
+   if  @board.attack(playerInput) == false
+       @remaining_misses -= 1
+       puts @remaining_misses.to_s
+   end
+
+   @board.print
+      
+   
+end
 
 
 
--- W2D3/battleship_project/lib/player.rb
+++ W2D3/battleship_project/lib/player.rb
@@ -2,7 +2,7 @@ class Player
     def get_move
         puts 'enter a position with coordinates separated with a space like `4 7`' 
         move = gets.chomp.split(' ')
-        move.map{|pos| pos.to_i}
+        move.map!{|pos| pos.to_i}
     end
 end
 
