5ddafe072d3177147156c32f75bab831f8c76e1a
Zhile Lin <zhlin04@gmail.com>
2022-04-27 11:13:15 -0700

    finished BOARD

--- W2D3/battleship_project/lib/board.rb
+++ W2D3/battleship_project/lib/board.rb
@@ -1,4 +1,7 @@
+require "byebug"
+
 class Board
+
 
     def initialize(n)
          @grid = Array.new(n){Array.new(n,:N)}
@@ -38,6 +41,90 @@ class Board
 
         return shipCount
     end
+# part 2
+
+# attack
+   def attack(pos)
+       
+       if self.[](pos) == :S  
+            self.[]=(pos, :H)
+            p 'you sunk my battleship!'
+            return true 
+       else
+         self.[]=(pos, :X)
+           return false
+       end
+
+    end
+#        if self[pos] == :S  
+#             self[pos]= :H
+#             p 'you sunk my battleship!'
+#             return true 
+#        else
+#          self[pos]= :X
+#            return false
+#        end
+       
+#    end
+   
+
+#place_random_ships
+   def  place_random_ships
+
+
+       shipNums = @size * 0.25
+       n = @grid.length
+       
+        while self.num_ships < shipNums
+            ranRow = rand(0...n)
+            ranCol = rand(0...n)
+            pos =[ranRow,ranCol]
+            self[pos] = :S 
+        end
+       
+   end
+ 
+#hidden_ships_grid
+     
+def hidden_ships_grid
+    copyCatGrid = Array.new(@grid.length){Array.new(@grid.length)}
+   copyCatGrid.each_index do |i|
+        copyCatGrid[i].each_index do |j|
+           if @grid[i][j] == :X 
+              copyCatGrid[i][j] = :X
+           else
+              copyCatGrid[i][j] = :N
+           end
+        end
+    end
+
+     copyCatGrid
+
+end
+
+
+# print_grid
+
+    def self.print_grid(grid)
+        grid.each do |row|
+            puts row.join(" ")
+        end
+        
+    end
+
+#cheat
+
+    def cheat
+       Board.print_grid(@grid)
+    end
+
+
+#print
+    def print 
+        Board.print_grid(hidden_ships_grid)
+    end
 
           
 end
+
+
