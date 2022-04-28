ce5e6cec2acfd8a0f81e4ebb2d5cf37f59db2ec8
Zhile Lin <zhlin04@gmail.com>
2022-04-28 02:03:32 -0700

    almost

--- W2D3/tic-tac-toe/Version1/game.rb
+++ W2D3/tic-tac-toe/Version1/game.rb
@@ -1,2 +1,30 @@
 require_relative "board.rb"
 require_relative "human_player.rb"
+
+class Game
+  
+def initialize(player_1,player_2)
+     @player_1 = HumanPlayer.new(player_1)
+     @player_2 = HumanPlayer.new(player_2)
+     @current_Player = @player_1
+     @board = Board.new
+     
+end
+
+
+def self.switch_turn
+
+    if @current_Player == @player_1
+        @current_Player = @player_2
+    else
+        @current_Player = @player_1
+    end
+
+end
+
+
+
+
+
+
+end
