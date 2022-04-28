3b4d5479e4a1732a5274bfabb1c8bb6a35a70951
Zhile Lin <zhlin04@gmail.com>
2022-04-28 01:24:49 -0700

    human class

--- W2D3/tic-tac-toe/Version1/human_player.rb
+++ W2D3/tic-tac-toe/Version1/human_player.rb
@@ -1,7 +1,27 @@
 class HumanPlayer
+
+
+attr_reader :mark
+
     def initialize(mark)
          @mark = mark
+        
+    end
+
+
+    def get_position
+       p "#{mark},pls enter a position in such format: row,col"
+       position = gets.chomp.split(',').map{|el| el.to_i}
+
+       if position.length != 2 
+        raise "sorry invalid input"
+       end
+
+       position 
 
     end
 
+     
+
 end
+
