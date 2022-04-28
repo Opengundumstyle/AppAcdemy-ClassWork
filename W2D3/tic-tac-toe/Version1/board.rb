7a2942fe6ae21aa59e46eef372eb1e4fe4a4e5b2
Zhile Lin <zhlin04@gmail.com>
2022-04-27 18:21:24 -0700

    finished board

--- W2D3/tic-tac-toe/Version1/board.rb
+++ W2D3/tic-tac-toe/Version1/board.rb
@@ -21,7 +21,7 @@ class Board
 
  end
 
-
+#*********************************************************************************
 #  This method should return a boolean indicating whether or not the specified position
 #  does not contain a player's mark.
 def empty?(pos)
@@ -35,6 +35,8 @@ def empty?(pos)
 end
 
 
+
+#*********************************************************************************
 # This method should assign the given mark to the specified position of the grid. 
 # It should raise an error when the position is not #valid? or not #empty?.
 def place_mark(pos, mark)
@@ -50,7 +52,98 @@ end
 
 
 
+#*********************************************************************************
+# This method should simply print out the board and all of the marks that have been 
+# placed on it. Feel free to style the printing as much as you'd like, but be 
+# sure to print out each row of the board on a new line. This will help us 
+# visualize the board better as we debug or even play the game!
 
+def print
+     @board.each do |row|
+       puts row.join(" ")
+     end
+
+end
+
+
+# This method should return a boolean indicating whether or not the given mark has 
+# completely filled up any row of the grid.
+
+def win_row?(mark)
+     @board.any? {|row| row.all?{|el| el == mark}}
+end
+
+
+# This method should return a boolean indicating whether or not the given mark has 
+# completely filled up any column of the grid
+def win_col?(mark)
+    (0...@board[0].length).each do |i|
+         win = 0
+        (0...@board.length).each do |j|
+
+            win += 1  if  @board[j][i] == mark
+
+         end 
+           return true if win ==  @board.length
+      end
+
+    false
+
+end
+
+
+# This method should return a boolean indicating whether or not the given mark has 
+# completely filled up either diagonal of the grid.
+
+
+def win_diagonal?(mark)
+     
+        i = 0
+        j = @board.length - 1 
+        k = 0
+        z = 0
+        diaWin = true
+
+   while  i < @board.length
+        diaWin = false if @board[i][j] != mark
+        i += 1
+        j -= 1
+    end
+
+    return true if diaWin == true
+    
+    diaWin = true
+
+    while  k < @board.length
+        diaWin = false if @board[z][k] != mark
+        k += 1
+        z += 1
+    end
+
+      diaWin
+
+end
+
+#This method should return a boolean indicating whether or not the given mark has 
+#filled any full row, column, or diagonal.
+
+def win?(mark)
+
+  return true if win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+
+  false
+
+end 
+
+
+# This method should return a boolean indicating whether or not there is at least 
+# one empty position on the grid.
+
+def empty_positions?
+    
+    @board.flatten.any?{|el| el == '_'}
+    
+end
 
 
 
