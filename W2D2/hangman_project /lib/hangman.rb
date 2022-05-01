b8a8782a97ae402368ef8c57ff96318873e6c0e6
Zhile Lin <zhlin04@gmail.com>
2022-05-01 16:15:06 -0700

    part 1 of hangman, pls hang me

--- W2D2/hangman_project /lib/hangman.rb
+++ W2D2/hangman_project /lib/hangman.rb
@@ -1,7 +1,57 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+    
+#part 1
+ 
+   attr_reader :guess_word , :attempted_chars , :remaining_incorrect_guesses
 
-   
+      def self.random_word
+           random = DICTIONARY.sample
+      end
+
+   def initialize
+       @secret_word = Hangman.random_word
+       @guess_word = Array.new(@secret_word.length,"_")
+       @attempted_chars = []
+       @remaining_incorrect_guesses = 5
+
+   end
+
+  def already_attempted?(char)
+
+    @attempted_chars.include?(char)
+
+  end
+
+  def get_matching_indices(char)
+
+      idxArr = []
+      (0...@secret_word.length).each do |i|
+          idxArr << i if @secret_word[i] == char
+      end
+
+       idxArr
+  end
+
+
+def fill_indices(char,arr)
+    
+   arr.each do |i|
+       @guess_word[i] = char
+   end
+
+end
+
+
+
+#part 2
+
+
+
+
+
+
+
 
 
 end
