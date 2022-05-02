eef9150a9e55ff896f8d056d6d2dde3abc22092f
Zhile Lin <zhlin04@gmail.com>
2022-05-01 21:33:01 -0700

    3rd time doing PA 2

--- FA2_P/lib/board.rb
+++ FA2_P/lib/board.rb
@@ -2,77 +2,14 @@ require "byebug"
 
 class Board
     
-    attr_reader :max_height
+  
     # This Board#print method is given for free and does not need to be modified
     # It is used to make your game playable.
     def print
         @stacks.each { |stack| p stack }
     end
 
-   def self.build_stacks(n)
-       newArr = Array.new(n){Array.new}
-   end
-
-   def initialize(stacks,maxheight)
-       if stacks < 4 || maxheight < 4
-           raise 'rows and cols must be >= 4'
-       end
   
-         @max_height = maxheight
-         @stacks = Board.build_stacks(stacks)
-
-   end
-
-
-   def add(token,idx)
-      if @stacks[idx].length < @max_height
-           @stacks[idx] << token
-           return true
-
-      else
-         return false
-   
-      end
-
-   end
-
-
-  def vertical_winner?(token)
-       @stacks.any?{|subArr| subArr.count{|el| el == token} == @max_height}
-      
-  end
-
-
-  def horizontal_winner?(token)
-        
-        #  (0...@max_height).each do |col|
-        #            streak = 0
-        #     (0...@stacks.length).each do |row|
-
-        #             streak += 1 if @stacks[row][col] == token
-        #     end
-
-        #      return true if streak == @stacks.length
-
-        #  end
-
-        #  false
-
-       (0...@max_height).each do |i|
-          return true if @stacks.all? {|stack| stack[i] == token}
-  end
-          
-          false
-  end
-
-
-  def winner?(token)
-    
-        vertical_winner?(token) || horizontal_winner?(token)
-
-  end
-
-
 
 
 
--- FA2_P/lib/connect_tac_toe.rb
+++ FA2_P/lib/connect_tac_toe.rb
@@ -3,31 +3,7 @@ require_relative 'player'
 
 class ConnectTacToe
    
-    def initialize(stacks,maxheight)
-
-        @board = Board.new(stacks,maxheight)
-        @player_1 = Player.new('y')
-        @player_2 = Player.new('b')
-        @current_player = @player_1
    
-    end
-
-    def switch_players!
-        if @current_player == @player_1
-            @current_player = @player_2
-        else
-            @current_player = @player_1
-        end
-    end
-
-    def play_turn
-
-        getIdx = @current_player.get_stack_index
-        @board.add(@current_player.token,getIdx)
-        
-        switch_players!
-    end
-
 
     # This ConnectTacToe#play method is given for free and does not need to be modified
     # It is used to make your game playable.
