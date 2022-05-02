8c24cab5cd196d0f1a67ce460ebcfe01eaa42dc5
Zhile Lin <zhlin04@gmail.com>
2022-05-01 17:32:12 -0700

    yes Another one

--- W2D2/hotel_project/lib/hotel.rb
+++ W2D2/hotel_project/lib/hotel.rb
@@ -1,7 +1,66 @@
 require_relative "room"
 
 class Hotel
- 
 
+  attr_reader :rooms
+
+   def initialize(name,hash)
+    @name = name
+    @rooms = {}
+    hash.each do |k,v|
+
+        @rooms[k] = Room.new(v)
+
+    end
+
+   end
+   
+  def name
+      @name.split(" ").map(&:capitalize).join(" ")
+  end
+
+
+  def room_exists?(room)
+
+    @rooms.keys.include?(room)
+
+  end
+
+
+  def check_in(person,room)
+         
+      if  room_exists?(room)
+
+          puts'check in successful' if  @rooms[room].add_occupant(person)
+
+          puts 'sorry, room is full'
+
+      else
+          puts 'sorry, room does not exist'
+      end
+
+  end
+
+
+  def has_vacancy?
+
+     @rooms.each do |k,v|
+        if v.available_space > 0
+            return true
+        end
+     end
+
+   false
+end
+
+
+def list_rooms
     
+    @rooms.each do |k,v|
+        puts   k +  v.available_space.to_s
+     end
+  
+end
+
+
 end
