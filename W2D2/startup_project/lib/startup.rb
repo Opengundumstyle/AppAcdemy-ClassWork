db4631a0758fee2ad9fa5b422690ce4849dea8f1
Zhile Lin <zhlin04@gmail.com>
2022-05-01 18:02:05 -0700

    start up is difficult

--- W2D2/startup_project/lib/startup.rb
+++ W2D2/startup_project/lib/startup.rb
@@ -1,9 +1,78 @@
 require "employee"
 
-
-
 class Startup
 
+    attr_reader :name,:funding,:salaries,:employees
+
+    def initialize(name,funding,salaries)
+         @name = name
+         @funding = funding 
+         @salaries = salaries
+         @employees = []
+
+    end
+
+   def valid_title?(title)
+
+    #   @salaries.has_key?(title)
+
+      return true if @salaries[title] 
+
+       false
+
+   end
   
+
+  def >(startup)
+     
+      self.funding > startup.funding
+
+  end
+
+  def hire(employee,title)
+      
+      if valid_title?(title)
+          @employees << Employee.new(employee,title)
+      else
+        raise "there is no such title!"
+      end
+
+  end
+
+
+  def size
+
+    @employees.length
+
+  end
+
+
+  def pay_employee(employee)
+      
+       salary = @salaries[employee.title]
+
+     if @funding > salary
+        employee.pay(salary)
+        @funding -= salary
+
+     else
+       raise "the company is a scam!"
+     end
+
+  end
+
+ def payday
+ 
+    @employees.each do |employee|
+        
+        pay_employee(employee)
+
+    end
+
+ end
+
+
+
+
 
 end
