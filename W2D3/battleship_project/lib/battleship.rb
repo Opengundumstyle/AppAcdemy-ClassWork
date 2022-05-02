1d6c1e2dac137bc9b9e1d068f270ac9a13b248ab
Zhile Lin <zhlin04@gmail.com>
2022-05-02 00:10:11 -0700

    done 3rd times...

--- W2D3/battleship_project/lib/battleship.rb
+++ W2D3/battleship_project/lib/battleship.rb
@@ -3,7 +3,63 @@ require_relative "player"
 
 class Battleship
 
+    attr_reader :board,:player
+
+def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+
+end
+
+def start_game
+
+    @board.place_random_ships
+    puts @board.num_ships
+    @board.print
+
+end
+
+def lose?
+    if @remaining_misses <= 0
+        puts 'you lose'
+        return true
+    else
+        return false
+    end
+
+end
 
 
+def win?
+   if @board.num_ships == 0
+    puts 'you win'
+    return true
+   else
+    return false
+   end
+end
+
+def game_over?
+   return true if win? || lose?
+
+   false
+end
+
+
+def turn
+    move = @player.get_move
+
+    if  @board.attack(move)
+      puts @board.print
+      puts @remaining_misses
+    else
+        @remaining_misses -= 1
+        puts @board.print
+        puts @remaining_misses
+
+    end
+
+end
 
 end
--- W2D3/battleship_project/lib/board.rb
+++ W2D3/battleship_project/lib/board.rb
@@ -1,8 +1,91 @@
 class Board
    
+  attr_reader :size
+def initialize(n)
+   @grid = Array.new(n){Array.new(n,:N)}
+   @size = n*n
+
+end
+
+def [](pos)
+
+row,collum = pos
+@grid[row][collum] 
+
+end
+
+def []=(pos,value)
+row,collum = pos
+@grid[row][collum] = value
+
+end
+
+
+def num_ships
+   @grid.map{|row| row.count(:S)}.sum
+end
 
 
 
+def attack(pos)
+
+  if self[pos] == :S 
+      self[pos] = :H
+      puts 'you sunk my battleship!' 
+      return true
+
+  else
+    self[pos] = :X
+    return false
+  end
+
+
+end
+
+
+def place_random_ships
+   totalship = @size * 0.25 
+   while num_ships < totalship
+         ranCol = rand(0...@grid.length)
+         ranRow = rand(0...@grid.length)
+         
+         pos= [ranRow,ranCol]
+
+         self[pos] = :S
+   end
+   
+end
+
+
+def hidden_ships_grid
+
+    newGrid = Array.new(@grid.length){Array.new(@grid.length)}
+    
+    @grid.each_index do |i|
+       @grid[i].each_index do |j|
+              if @grid[i][j] == :X 
+            newGrid[i][j] = :X
+              else
+                newGrid[i][j] = :N
+            end
+       end
+    end
+
+      newGrid
+
+end
+
+def self.print_grid(grid)
+   grid.each {|row| puts row.join(" ")}
+end
+
+def cheat
+    Board.print_grid(@grid)
+end
+
+def print
+  Board.print_grid(hidden_ships_grid)
+end
 
 
 end
