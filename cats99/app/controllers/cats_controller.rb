ac78c871dbeb65273e4e5dbe165f1f51046eea39
Zhile Lin <zhlin04@gmail.com>
2022-05-31 00:37:56 -0700

    finished edit form

--- cats99/app/controllers/cats_controller.rb
+++ cats99/app/controllers/cats_controller.rb
@@ -1,7 +1,6 @@
 class CatsController < ApplicationController
     def index
         @cats = Cat.all
-
         render :index
     end
 
@@ -31,22 +30,21 @@ class CatsController < ApplicationController
     end   
 
   def edit
-
+       @colors = Cat.catcolors
+       @cat = Cat.find_by(id: params[:id])
+       render :edit
   end
 
   def update
+      @cat = Cat.find_by(id: params[:id])
+
+      if @cat.update_attributes(cat_params)
+          redirect_to cat_url(@cat)
+      else
+        render :edit
+      end
   end
    
-
-
-
-
-
-
-
-
-
-
     private 
     def cat_params
         params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
